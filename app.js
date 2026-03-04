835404abb61afeecb855e9c083e78985c642d645
CodingWithCuba <mulwelim37@gmail.com>
2026-03-04 11:23:56 +0200

    https://github.com/MukhubaM/My-Portfolio.github.io

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 const hamb = document.querySelector('.header .nav-bar .nav-list .hamb');
- const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
- const header = document.querySelector('.header.container');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const header = document.querySelector('.header.container');
+const menu_items = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 
- hamb.addEventListener('click', () => {
+hamb.addEventListener('click', () => {
 	hamb.classList.toggle('active');
 	mobile_menu.classList.toggle('active');
 });
 
 document.addEventListener('scroll', () => {
-    var scroll_position = window.scrollY;
-    if(scroll_position = header){
-        header.style.backgroundColor = '#28a9ee';
-    }else{
-        header.style.backgroundColor = 'transparent';
-    }
+	var scroll_position = window.scrollY;
+
+	if (scroll_position > 50) {
+		header.style.backgroundColor = '#28a9ee';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
 });
 
-menu_item.forEach((item) => {
+menu_items.forEach((item) => {
 	item.addEventListener('click', () => {
-		hamburger.classList.toggle('active');
-		mobile_menu.classList.toggle('active');
+		hamb.classList.remove('active');
+		mobile_menu.classList.remove('active');
 	});
 });
